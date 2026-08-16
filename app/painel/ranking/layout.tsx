4aa2e3238abd87efa16ab329b6b7c344a5bf8e8b
luanpatrickbo444 <luanpatrickbo4@gmail.com>
2026-08-16 00:06:06 -0400

    one menu

--- app/painel/ranking/layout.tsx
+++ app/painel/ranking/layout.tsx
@@ -1,5 +1,3 @@
-import { PanelSectionShell } from '@/components/PanelSectionShell'
-
 export default function SectionLayout({ children }: { children: React.ReactNode }) {
-  return <PanelSectionShell>{children}</PanelSectionShell>
+  return children
 }
